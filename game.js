97b3892f8e72931a11deddd633acf2a2af5597cd
Blizzos <177258855+Blizzos@users.noreply.github.com>
2026-05-12 16:37:55 +0200

    updated code in js and html

--- game.js
+++ game.js
@@ -8,3 +8,32 @@ const shopItems = [
     }
 ];
 
+const savedItems = localStorage.getItem('shopItems');
+if (savedItems) {
+    const parsed = JSON.parse(savedItems);
+    parsed.forEach(function(savedItem, index) {
+        shopItems[index].count = savedItem.count;
+    });
+}
+
+const catImage = document.getElementById('cat');
+const petCountDisplay = document.getElementById('pet-count');
+const multiplierDisplay = document.getElementById('multiplier');
+
+let pets = Number(localStorage.getItem('pets')) || 0;
+
+function buildShop() {
+    const shop = document.getElementById('shop');
+    shop.innerHTML = '<h2>Shop</h2>';
+
+    shopItems.forEach(function(item) {
+        shop.innerHTML += `
+        <div class="shop-item">
+        <button id="buy-${item.id}">
+        ${item.name} - ${item.cost} pets
+        </button>
+        <span id="count-${item.id}">${item.count}</span>
+        </div>
+        `;
+    });
+}
